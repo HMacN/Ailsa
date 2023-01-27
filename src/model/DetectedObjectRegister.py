222450956132d101135bd046758c25b18defabea
HMacN <hugh.macn@gmail.com>
2023-01-27 05:01:31 +0000

    DetectedObjectRegister Refactored.
    Changes:
    
    * Refactored the class using an internal function.

--- src/model/DetectedObjectRegister.py
+++ src/model/DetectedObjectRegister.py
@@ -12,60 +12,36 @@ class DetectedObjectRegister:
             self.__list_of_items.append(item)
 
     def get_by_name(self, name: str) -> list:
+        def selection_function(item: IdentifiedObject) -> bool:
+            return item.get_object_name() == name
 
-        list_to_return = list()
-
-        for i in range(len(self.__list_of_items)):
-            if self.__list_of_items[i].get_object_name() == name:
-                list_to_return.append(self.__list_of_items[i])
-
-        return list_to_return
+        return self.__get_sublist(selection_function)
 
     def get_between_horiz_origin_and(self, horiz_coord: int) -> list:
-        list_to_return = list()
+        def selection_function(item: IdentifiedObject) -> bool:
+            return (item.horizontal_distance_to_origin + item.bounding_box_width) <= horiz_coord
 
-        for i in range(len(self.__list_of_items)):
-            item: IdentifiedObject = self.__list_of_items[i]
-
-            if (item.horizontal_distance_to_origin + item.bounding_box_width) <= horiz_coord:
-                list_to_return.append(item)
-
-        return list_to_return
+        return self.__get_sublist(selection_function)
 
     def get_between_horiz_far_edge_and(self, horiz_coord: int) -> list:
-        list_to_return = list()
+        def selection_function(item: IdentifiedObject) -> bool:
+            return item.horizontal_distance_to_origin >= horiz_coord
 
-        for i in range(len(self.__list_of_items)):
-            item: IdentifiedObject = self.__list_of_items[i]
-
-            if item.horizontal_distance_to_origin >= horiz_coord:
-                list_to_return.append(item)
-
-        return list_to_return
+        return self.__get_sublist(selection_function)
 
     def get_between_vert_origin_and(self, vert_coord: int) -> list:
-        list_to_return = list()
+        def selection_function(item: IdentifiedObject) -> bool:
+            return (item.vertical_distance_to_origin + item.bounding_box_height) <= vert_coord
 
-        for i in range(len(self.__list_of_items)):
-            item: IdentifiedObject = self.__list_of_items[i]
-
-            if (item.vertical_distance_to_origin + item.bounding_box_height) <= vert_coord:
-                list_to_return.append(item)
-
-        return list_to_return
+        return self.__get_sublist(selection_function)
 
     def get_between_vert_far_edge_and(self, vert_coord: int) -> list:
-        list_to_return = list()
+        def selection_function(item: IdentifiedObject) -> bool:
+            return item.vertical_distance_to_origin >= vert_coord
 
-        for i in range(len(self.__list_of_items)):
-            item: IdentifiedObject = self.__list_of_items[i]
+        return self.__get_sublist(selection_function)
 
-            if item.vertical_distance_to_origin >= vert_coord:
-                list_to_return.append(item)
-
-        return list_to_return
-
-    def __get_sublist(self, selection_function) -> list:    # todo still working on this.
+    def __get_sublist(self, selection_function) -> list:
         list_to_return = list()
 
         for i in range(len(self.__list_of_items)):
