1954291ac9f162f66623f0b83b2580225d392d50
HMacN <hugh.macn@gmail.com>
2023-04-09 00:59:39 +0100

    KnowledgeUnit.py Now Counts Seen Items.
    * Added functionality to KnowledgeUnit.py to allow it to return the number of items of a particular kind it has seen.

--- src/model/KnowledgeUnit.py
+++ src/model/KnowledgeUnit.py
@@ -1,16 +1,49 @@
 from util.BoundingBoxCollection import BoundingBoxCollection
+from util.SafeListEditor import safely_remove_list_indexes as safe_del
 
 
 class KnowledgeUnit:
 
     def __init__(self):
         self.__seen_items__: list = list()
+        self.__item_counts__: list = list()
 
     def get_seen_items(self) -> list:
         return sorted(self.__seen_items__)
 
-    def add_frame(self, frame: BoundingBoxCollection):
-        for box in frame:
-            label = box.label
-            if not self.__seen_items__.__contains__(label):
-                self.__seen_items__.append(label)
+    def add_frame(self, frame_boxes: BoundingBoxCollection):
+
+        frame: KnowledgeUnit.Frame = KnowledgeUnit.Frame(frame_boxes)
+
+        for i in range(len(frame.item_types)):
+            item = frame.item_types[i]
+            count = frame.item_counts[i]
+            if not self.__seen_items__.__contains__(item):
+                self.__seen_items__.append(item)
+                self.__item_counts__.append(count)
+            else:
+                index = self.__seen_items__.index(item)
+                if self.__item_counts__[index] < count:
+                    self.__item_counts__[index] = count
+
+    def have_you_seen_a(self, item_label: str) -> int:
+        for i in range(len(self.__seen_items__)):
+            if self.__seen_items__[i] == item_label:
+                return self.__item_counts__[i]
+        return 0
+
+    class Frame:
+        def __init__(self, bboxes: BoundingBoxCollection):
+            self.bboxes: BoundingBoxCollection = bboxes
+            self.item_types: list = list()
+            self.item_counts: list = list()
+
+            for box in bboxes:
+                label = box.label
+                if not self.item_types.__contains__(label):
+                    self.item_types.append(label)
+                    self.item_counts.append(1)
+                else:
+                    index = self.item_types.index(label)
+                    self.item_counts[index] += 1
+
--- tests/testKnowledgeUnit.py
+++ tests/testKnowledgeUnit.py
@@ -40,4 +40,93 @@ class KnowledgeUnitTests(unittest.TestCase):
                          msg="The expected value: \n" + str(expected_results) +
                          "\ndid not equal the actual value: \n" + str(actual_results))
 
+    def test_remembers_seen_items_across_multiple_frames(self):
+        ku = KnowledgeUnit()
+
+        frame_1 = BoundingBoxCollection()
+        frame_1.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "A"))
+        ku.add_frame(frame_1)
+
+        frame_2 = BoundingBoxCollection()
+        frame_2.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "B"))
+        ku.add_frame(frame_2)
+
+        frame_3 = BoundingBoxCollection()
+        frame_3.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "C"))
+        ku.add_frame(frame_3)
+
+        expected_results: list = ["A", "B", "C"]
+        actual_results: list = ku.get_seen_items()
+
+        self.assertEqual(expected_results, actual_results,
+                         msg="The expected value: \n" + str(expected_results) +
+                         "\ndid not equal the actual value: \n" + str(actual_results))
+
+    def test_have_you_seen_an_item_returns_1(self):
+        ku = KnowledgeUnit()
+
+        frame_1 = BoundingBoxCollection()
+        frame_1.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "A"))
+        ku.add_frame(frame_1)
+
+        frame_2 = BoundingBoxCollection()
+        frame_2.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "B"))
+        ku.add_frame(frame_2)
+
+        frame_3 = BoundingBoxCollection()
+        frame_3.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "C"))
+        ku.add_frame(frame_3)
+
+        expected_results: int = 1
+        actual_results: int = ku.have_you_seen_a("B")
+
+        self.assertEqual(expected_results, actual_results,
+                         msg="The expected value: \n" + str(expected_results) +
+                         "\ndid not equal the actual value: \n" + str(actual_results))
+
+    def test_have_you_seen_an_item_returns_0(self):
+        ku = KnowledgeUnit()
+
+        frame_1 = BoundingBoxCollection()
+        frame_1.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "A"))
+        ku.add_frame(frame_1)
+
+        frame_2 = BoundingBoxCollection()
+        frame_2.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "B"))
+        ku.add_frame(frame_2)
+
+        frame_3 = BoundingBoxCollection()
+        frame_3.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "C"))
+        ku.add_frame(frame_3)
+
+        expected_results: int = 0
+        actual_results: int = ku.have_you_seen_a("D")
+
+        self.assertEqual(expected_results, actual_results,
+                         msg="The expected value: \n" + str(expected_results) +
+                         "\ndid not equal the actual value: \n" + str(actual_results))
+
+    def test_have_you_seen_item_returns_count_of_max_number_seen_at_one_time(self):
+        ku = KnowledgeUnit()
+
+        frame_1 = BoundingBoxCollection()
+        frame_1.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "B"))
+        ku.add_frame(frame_1)
+
+        frame_2 = BoundingBoxCollection()
+        frame_2.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "B"))
+        frame_2.add(Box(0.0, 0.1, 0.0, 0.1, 0.5, "B"))
+        ku.add_frame(frame_2)
+
+        frame_3 = BoundingBoxCollection()
+        frame_3.add(Box(0.2, 0.3, 0.2, 0.6, 0.5, "B"))
+        ku.add_frame(frame_3)
+
+        expected_results: int = 2
+        actual_results: int = ku.have_you_seen_a("B")
+
+        self.assertEqual(expected_results, actual_results,
+                         msg="The expected value: \n" + str(expected_results) +
+                         "\ndid not equal the actual value: \n" + str(actual_results))
+
 
