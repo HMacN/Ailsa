222908dbf27af98118cbcdf7f7a28967bdea296b
HMacN <hugh.macn@gmail.com>
2023-04-13 14:49:13 +0100

    Added DocStrings to Frame.py
    * Added DocStrings to Frame.py for all functions and internal classes.

--- src/cv2wrapper/Frame.py
+++ src/cv2wrapper/Frame.py
@@ -7,25 +7,68 @@ from util.Box import Box
 
 
 class Frame:
+    """
+    A wrapper class to hold all image recording and encoding.  So far this project uses two image formats, PIL and
+    numpy.ndarray.  This class should allow for easy use of whichever image format is required.
+
+    Note that to improve performance, both forms of image are generated on class instantiation, and then passed to the
+    user as required, rather than doing multiple conversions between image formats.
+    """
     def __init__(self, pil_image: Image, cv2_image: numpy.ndarray):
+        """
+        The 'true' constructor for this class.  As it requires the same image in both formats, it is envisioned that
+        this function is not called by the user, and instead the two 'new_from_...' functions should be used to
+        instantiate this class from a single image in whichever format is available.
+
+        @param pil_image: A PIL.Image object which is the image to store.
+        @param cv2_image: A numpy.ndarray which is the image to store.
+        """
         self.__cv2_img__: numpy.ndarray = cv2_image
         self.__pil_img__: Image = pil_image
 
     @classmethod
     def new_from_pil(cls, pil_image: Image):
+        """
+        A constructor function which generates an instance of this class from a PIL.Image object.
+
+        @param pil_image: A PIL.Image object to create a Frame object from.
+        @return:
+        """
         return cls(pil_image, cls.__cv2_from_pil__(pil_image))
 
     @classmethod
     def new_from_cv2(cls, cv2_image: numpy.ndarray):
+        """
+        A constructor function which generates an instance of this class from a numpy.ndarray object.
+
+        @param cv2_image: A numpy.ndarray object (the CV2 image format) to create a Frame object from.
+        @return:
+        """
         return cls(cls.__pil_from_cv2__(cv2_image), cv2_image)
 
     def get_pil_img(self) -> Image:
+        """
+        A getter for the PIL.Image form of the image in this frame.
+
+        @return: A PIL.Image object which is the picture in this Frame.
+        """
         return self.__pil_img__
 
     def get_cv2_img(self) -> numpy.ndarray:
+        """
+        A getter for the numpy.ndarray object (the CV2 image format) form of the image in this frame.
+
+        @return: A numpy.ndarray object (the CV2 image format) which is the picture in this Frame.
+        """
         return self.__cv2_img__
 
     def draw_bounding_boxes(self, box_collection: BoundingBoxCollection):
+        """
+        Draw the given set of bounding boxes onto the picture in this frame.
+
+        @param box_collection:  A BoundingBoxCollection object which contains the boxes to draw onto the Frame.
+        @return:
+        """
         colors = list(ImageColor.colormap.values())
         font = ImageFont.load_default()
         pil_img = self.__pil_img__
@@ -51,7 +94,20 @@ class Frame:
                                        font,
                                        thickness=4,
                                        display_str_list=()):
-        """Adds a bounding box to an image."""
+        """
+        The function to actually draw a single bounding box onto the image.  Note that this can only happen in PIL
+        format, so the cv2 format image will need drawn on elsewhere.
+
+        @param y_min:               The minimum y coordinate of the box.
+        @param x_min:               The minimum x coordinate of the box.
+        @param y_max:               The maximum y coordinate of the box.
+        @param x_max:               The maximum x coordinate of the box.
+        @param color:               The colour of the box.
+        @param font:                The font to be used for the label of the box.
+        @param thickness:           The thickness of the lines the box is to be drawn with.
+        @param display_str_list:    A list of str values to print to the label of the box.
+        @return:
+        """
         draw = ImageDraw.Draw(self.__pil_img__)
         im_width, im_height = self.__pil_img__.size
         (left, right, top, bottom) = (x_min * im_width, x_max * im_width,
@@ -92,8 +148,20 @@ class Frame:
 
     @staticmethod
     def __pil_from_cv2__(cv2_image: numpy.ndarray) -> Image:
+        """
+        A function to convert a numpy.ndarray (cv2 image format) to a PIL.
+
+        @param cv2_image:   The numpy.ndarray object that is to be converted to a PIL.
+        @return:            A PIL.Image object which was created from the numpy.ndarray.
+        """
         return Image.fromarray(cv2.cvtColor(cv2_image, cv2.COLOR_BGR2RGB))
 
     @staticmethod
     def __cv2_from_pil__(pil_image: Image) -> numpy.ndarray:
+        """
+        A function to convert a PIL to a numpy.ndarray (cv2 image format).
+
+        @param pil_image:   The PIL.Image object that is to be converted to a numpy.ndarray.
+        @return:            A numpy.ndarray object which was created from the PIL.
+        """
         return cv2.cvtColor(numpy.array(pil_image), cv2.COLOR_RGB2BGR)
