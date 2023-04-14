ac85c8f57b143f3c9fdcad2fa497d8ba2ee77a4d
HMacN <hugh.macn@gmail.com>
2023-04-14 22:02:48 +0100

    Added DocStrings to SafeListEditor.py
    * Added DocStrings to SafeListEditor.py for all functions and internal classes.

--- src/util/SafeListEditor.py
+++ src/util/SafeListEditor.py
@@ -1,4 +1,11 @@
 def safely_remove_list_indexes(list_to_edit, list_of_indexes: list) -> list:
+    """
+    A helper function to safely remove a list of indices from a list.
+
+    @param list_to_edit:        A list object to remove values from.
+    @param list_of_indexes:     A list of int values which are the indices to remove in the given list.
+    @return:                    The given list, with the relevant values removed.
+    """
 
     for index in sorted(list_of_indexes, reverse=True):
         if 0 <= index < len(list_to_edit):
