6ce96643522cb9696dcf218fd8f6002997ac2546
HMacN <hugh.macn@gmail.com>
2023-04-14 21:53:28 +0100

    Added DocStrings to Box.py
    * Added DocStrings to Box.py for all functions and internal classes.

--- src/util/Box.py
+++ src/util/Box.py
@@ -1,6 +1,12 @@
 
 
-def __get_as_str__(string):
+def __get_as_str__(string) -> str:
+    """
+    A helper function to make sure that a given string is in str format, and not a bytestring.
+
+    @param string:  A string that may or may not be a bytestring.
+    @return:        A str, which is the same as the given string.
+    """
     if isinstance(string, (bytes, bytearray)):
         return str(string, "utf-8")
     elif isinstance(string, str):
@@ -10,9 +16,22 @@ def __get_as_str__(string):
 
 
 class Box:
+    """
+    A class to describe a single bounding box, along with some details and helpful functions.
+    """
 
     def __init__(self, left_edge: float, right_edge: float,
                  lower_edge: float, upper_edge: float, confidence: float, label: str):
+        """
+        The constructor.
+
+        @param left_edge:   A float, which is the left edge, expressed as a decimal of the width of the frame.
+        @param right_edge:  A float, which is the right edge, expressed as a decimal of the width of the frame.
+        @param lower_edge:  A float, which is the lower edge, expressed as a decimal of the height of the frame.
+        @param upper_edge:  A float, which is the upper edge, expressed as a decimal of the height of the frame.
+        @param confidence:  A float, which is the detection confidence, on a scale of 0.0 (doubt) to 1.0 (certainty).
+        @param label:       A str, which is the classification of this item.
+        """
         self.left_edge: float = left_edge
         self.right_edge: float = right_edge
         self.lower_edge: float = lower_edge
@@ -21,6 +40,11 @@ class Box:
         self.label: str = __get_as_str__(label)
 
     def __str__(self) -> str:
+        """
+        An override of the str function.  Returns a string which summarises the contents of the box.
+
+        @return:    A str which summarises the contents of the box.
+        """
         left = "left: " + str(self.left_edge)
         right = "right: " + str(self.right_edge)
         lower = "lower: " + str(self.lower_edge)
@@ -32,6 +56,13 @@ class Box:
         return left + c + right + c + lower + c + upper + c + conf + c + label
 
     def get_overlap_area(self, overlapping_box: 'Box') -> float:
+        """
+        Returns the absolute overlap area that this box shares with another box.  Returns 0.0 if the boxes do not
+        overlap.
+
+        @param overlapping_box: A Box object that this box may overlap with.
+        @return:                A float which is the overlap area between the two boxes.
+        """
         no_overlap = 0.0
 
         if self.left_edge > overlapping_box.right_edge:
@@ -53,6 +84,13 @@ class Box:
         return overlap_area
 
     def get_iou(self, other_box: 'Box') -> float:
+        """
+        Returns the Intersection over Union factor for this box and the given box.  This is a commonly used metric in
+        image detection systems.  Returns 0.0 if the boxes do not overlap.
+
+        @param other_box:   A Box object with which to get the IoU factor of this box with.
+        @return:            A float, which is the IoU factor of the given box with this one.
+        """
         intersection_area = self.get_overlap_area(other_box)
 
         # Save time here by returning 0 if intersection area is effectively zero.
@@ -67,5 +105,10 @@ class Box:
         return intersection_over_union
 
     def get_area(self) -> float:
+        """
+        Gets the area of the box.
+
+        @return: A float, which is the area of this box.
+        """
         return (self.right_edge - self.left_edge) * (self.upper_edge - self.lower_edge)
 
